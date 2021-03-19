0c9fe09fa3b85f645d2f06aa7f1c09ad5c2c37e9
Benni <benni@karafi.at>
2021-03-19 17:37:50 +0100

    read_record function to read input from msft.csv

--- main.cpp
+++ main.cpp
@@ -1,23 +1,68 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
+struct Stock
+{
+    string open, high, low, close ,adjclose, volume;
+    string date;
 
-void saveCSV() {
+};
 
-    ofstream myfile;
-    myfile.open ("example.csv");
-    myfile << "This is the first cell in the first column.\n";
-    myfile << "a,b,c,\n";
-    myfile << "c,s,v,\n";
-    myfile << "1,2,3.456\n";
-    myfile << "semi;colon";
-    myfile.close();
+void create()
+{
+    //import function comes here
 }
 
-void loadCSV() {
+void read_record()
+{
+    fstream file;  // file stream
 
+    // open a CSV file for input
+    file.open("MSFT.csv", ios::in);
+
+    // if file is open then display the contents
+    if (file)
+    {
+
+        Stock currStock; // one data record
+        std::string input; //holds input of file as string
+
+
+        //display report header
+        std::cout << endl << std::left << std::setw(12) << "Date"
+                << std::left << std::setw(12) << "OPEN"
+                << std::left << std::setw(12) << "HIGH"
+                << std::left << std::setw(12) << "LOW"
+                << std::left << std::setw(12) << "CLOSE"
+                << std::left << std::setw(12) << "ADJ CLOSE"
+                << std::left << std::setw(12) << "VOLUME"
+                << std::endl;
+
+
+        //until reach end-of-file, read and display each record
+        while (std::getline(file, input))
+        {
+
+            std::getline(file, currStock.date, ',');
+            std::getline(file, currStock.open, ',');
+            std::getline(file, currStock.high, ',');
+            std::getline(file, currStock.low, ',');
+            std::getline(file, currStock.close, ',');
+            std::getline(file, currStock.adjclose, ',');
+            std::getline(file, currStock.volume);
+
+            cout << currStock.date << "  " << currStock.open << "  " << currStock.high << "  " << currStock.low << "  " << currStock.close << "  " << currStock.adjclose << "  " << currStock.volume << endl;
+
+        }
+    }
+    else // could not open the file, tell the user
+    {
+        cout << "Error opening file.\n";
+    }
 }
 
 char MenuInput(char InputValue) {
@@ -48,11 +93,12 @@ char MenuInput(char InputValue) {
         }
         case '6': {
             //SAVE
-            saveCSV();
+            create();
             break;
         }
         case '7': {
             //LOAD
+            read_record();
             break;
         }
         case '8': {
